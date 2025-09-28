82fadc7bc5725c329f770b677f6365f49d5bf297
Brett Gamble <brettkgamble@gmail.com>
2025-09-28 14:08:49 -0600

    0082 - cv about

--- apps/web/src/app/cv/page.tsx
+++ apps/web/src/app/cv/page.tsx
@@ -1,7 +1,29 @@
+import { client } from "../../lib/sanity/client";
 import { SanityImage } from "../../components/sanity-image";
+import { groq } from 'next-sanity';
+
+const query = groq `
+  *[_type=='author'] {
+        name,
+        image{
+        ...,
+        ...asset->{
+          "alt": coalesce(altText, originalFilename, "no-alt"),
+          "blurData": metadata.lqip,
+          "dominantColor": metadata.palette.dominant.background
+        }
+      },
+    }
+  `
+
+const p = await client.fetch(query)
+const person = p[0];
+
+
 
 export default async function Page() {
 
+console.log('Person', person)
   return (
     <div className="flex flex-wrap">
       <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
@@ -10,17 +32,20 @@ export default async function Page() {
               text-start tracking-widest  ">ABOUT ME
               <hr className="mt-2 w-64 h-2 dark:bg-blue-700"/>
           </h1>
-        </div>
-        {/* <SanityImage
-                asset={image}
-                alt={title ?? "Blog post image"}
-                fill
-                priority={false}
-                loading="lazy"
-                quality={80}
-                objectFit="cover"
-                className="rounded-2xl"
-              /> */}
+          <div className="relative flex flex-col justify-center py-16 px-16 sm:w-4/5 ">
+            <SanityImage
+              asset={person.image}
+              // alt={title ?? "Blog post image"}
+              fill
+              width="300"
+              height="300"
+              priority={false}
+              loading="lazy"
+              quality={100}
+              className="mx-auto w-1/3 rounded-full sm:w-2/5 md:w-3/4 lg:w-2/4"
+            />
+            </div>
+            </div>
       </div>
   );
 }
