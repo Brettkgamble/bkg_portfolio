4cd46b8c1c133a90b08d2271c8e0c14e5ac3119a
Brett Gamble <brettkgamble@gmail.com>
2025-11-01 13:34:19 -0600

    0083 - Centered the SanityImage component for the headshot in cv./page.tsx

--- apps/web/src/app/cv/page.tsx
+++ apps/web/src/app/cv/page.tsx
@@ -27,22 +27,22 @@ console.log('Person', person)
   return (
     <div className="flex flex-wrap">
       <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
-      <div className=" flex-none pt-8 pl-2 w-11/12 md:w-1/5 lg:w-1/6">
+      <div className=" flex-none pt-8 pl-2 ">
           <h1 className="pt-4 pl-4 font-roboto-600 font-bold text-3xl w-full uppercase
               text-start tracking-widest  ">ABOUT ME
               <hr className="mt-2 w-64 h-2 dark:bg-blue-700"/>
           </h1>
-          <div className="relative flex flex-col justify-center py-16 px-16 sm:w-4/5 ">
+          <div className="relative flex flex-col justify-center items-center py-8 w-3/5 mx-auto">
             <SanityImage
               asset={person.image}
               // alt={title ?? "Blog post image"}
-              fill
+              // fill
               width="300"
               height="300"
               priority={false}
               loading="lazy"
               quality={100}
-              className="mx-auto w-1/3 rounded-full sm:w-2/5 md:w-3/4 lg:w-2/4"
+              className="mx-auto w-full rounded-full "
             />
             </div>
             </div>
